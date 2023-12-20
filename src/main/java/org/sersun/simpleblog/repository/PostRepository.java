e55bbb74c948a24c2c22116e32f35d4c55bb71e2
Denis Sersun <d.sersun@gmail.com>
2023-12-20 23:50:44 +0200

    temporary deleted unnecessary code

--- src/main/java/org/sersun/simpleblog/repository/PostRepository.java
+++ src/main/java/org/sersun/simpleblog/repository/PostRepository.java
@@ -9,13 +9,4 @@ import java.util.List;
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
 
-    Post findByPostId(Long postId);
-
-    List<Post> findAllByUserUserId(Long userId);
-
-    List<Post> findAllByPublicationStatusName(String publicationStatus);
-    List<Post> findAllByUserUserIdAndPublicationStatusName(Long userId, String publicationStatus);
-
-    List<Post> findAllByTagListContains(String tag);
-
 }
