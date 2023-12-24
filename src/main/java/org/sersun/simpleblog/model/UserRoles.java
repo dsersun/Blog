716d5373235cd3946310b06c16cb52e3aa47bfbd
Denis Sersun <d.sersun@gmail.com>
2023-12-24 13:00:54 +0200

    new futures

---
+++ src/main/java/org/sersun/simpleblog/model/UserRoles.java
@@ -0,0 +1,51 @@
+package org.sersun.simpleblog.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+
+import java.util.Objects;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name="userroles")
+public class UserRoles {
+
+    @Id
+    @Column(name = "user_role_id")
+    private Long userRoleId;
+
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(name = "role_id")
+    private int roleId;
+
+    @Override
+    public String toString() {
+        return "UserRoles{" +
+                "userRoleId=" + userRoleId +
+                ", userId=" + userId +
+                ", roleId=" + roleId +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserRoles that = (UserRoles) o;
+        return Objects.equals(userRoleId, that.userRoleId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userRoleId);
+    }
+}
--- src/main/java/org/sersun/simpleblog/service/UsersService.java
+++ src/main/java/org/sersun/simpleblog/service/UsersService.java
@@ -1,11 +1,15 @@
 package org.sersun.simpleblog.service;
 
 import lombok.extern.slf4j.Slf4j;
+import org.sersun.simpleblog.model.UserRoles;
 import org.sersun.simpleblog.model.Users;
 import org.sersun.simpleblog.repository.UserRepository;
+import org.sersun.simpleblog.repository.UserRoleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 @Slf4j
@@ -14,6 +18,8 @@ public class UsersService {
     BCryptPasswordEncoder bCryptPasswordEncoder;
     @Autowired
     UserRepository userRepository;
+    @Autowired
+    UserRoleRepository userRoleRepository;
 
     public void createUser(Users user){
         try {
@@ -22,8 +28,13 @@ public class UsersService {
             user.setPassword(encodedPassword);
             userRepository.save(user);
             log.info("User {} created", user.getUsername());
+
+            // assign reader role to new user
+            userRoleRepository.save(new UserRoles(null, user.getUserId(), 4));
+            
         }catch (Exception e){
             log.error("Error creating user {}", user.getUsername(), e);
         }
     }
+
 }
